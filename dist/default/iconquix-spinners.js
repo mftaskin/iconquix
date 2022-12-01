d9d06bcf70b3090a2202100257bbaef5951fcc70
Fatih Taşkın <mfatih.taskin@gmail.com>
2022-12-01 23:02:38 +0300

    fix(struct): file structure redesigned

--- dist/default/iconquix-spinners.js
+++
@@ -1,8 +0,0 @@
-const iconQuixDataSpinners  = {
-    'spinner-circle':'<path d="m18 6a1 1 0 01-12 12 8.5 8.5 0 010-12" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />',
-    'spinner-comet':'<path d="m22 12a1 1 0 00-20 0 10 10 0 0020 0 8 8 0 01-17.8 0 1 1 0 0113.8 0 1 1 0 004 0zm-3.25 0a1 1 0 002.5 0 1 1 0 00-2.5 0z" fill="currentColor" fill-rule="evenodd" />',
-    'spinner-comet-double':'<path d="m22 12a1 1 0 00-20 0 1 1 0 0116 0 1 1 0 004 0zm-20 0a1 1 0 0020 0 1 1 0 01-16 0 1 1 0 00-4 0z" fill="currentColor" fill-rule="evenodd" />',
-    'spinner-dots':'<path d="m14.56 4.32a1 1 0 00-5.12 0 1 1 0 005.12 0zm-10.31 2.25a1 1 0 004.64 0 1 1 0 00-4.64 0zm2.17 5.43a1 1 0 00-4.2 0 1 1 0 004.2 0zm2.05 5.43a1 1 0 00-3.8 0 1 1 0 003.8 0zm5.25 2.25a1 1 0 00-3.44 0 1 1 0 003.44 0zm5.27-2.25a1 1 0 00-3.12 0 1 1 0 003.12 0zm2.1-5.43a1 1 0 00-2.82 0 1 1 0 002.82 0zm-2.09-5a1 1 0 00-2.5 0 1 1 0 002.5 0z" fill="currentColor" fill-rule="evenodd" />',
-    'spinner-lines': '<path d="m12 2v4m0 16v-4m-10-6h4m16 0h-4m-13.07-7.07 2.82 2.82m-2.82 11.32 2.82-2.82m11.32 2.82-2.82-2.82m2.82-11.32-2.82 2.82" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />',
-    'spinner-twins':'<path d="m2 12a10 10 0 0110-10v3a7 7 0 00-7 7zm10 10a10 10 0 0010-10h-3a7 7 0 01-7 7z" fill="currentColor" fill-rule="evenodd" />',
-};
--- dist/iconquix.js
+++ dist/iconquix.js
@@ -1,13 +1,3 @@
-const iconQuixData = {
-    'noicon-selected': '<path d="m6 4a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2v-12a2 2 0 00-2-2zm-4-2 20 20m0-20-20 20" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />',
-    ...((typeof iconQuixDataBasic !== 'undefined') ? iconQuixDataBasic : {}),
-    ...((typeof iconQuixDataArrows !== 'undefined') ? iconQuixDataArrows : {}),
-    ...((typeof iconQuixDataBrands !== 'undefined') ? iconQuixDataBrands : {}),
-    ...((typeof iconQuixDataFilesFolders !== 'undefined') ? iconQuixDataFilesFolders : {}),
-    ...((typeof iconQuixDataSpinners !== 'undefined') ? iconQuixDataSpinners : {}),
-    ...((typeof iconQuixDataVehicles !== 'undefined') ? iconQuixDataVehicles : {}),
-    ...((typeof iconQuixDataWeather !== 'undefined') ? iconQuixDataWeather : {}),
-};
 class iconQuix extends HTMLElement {
     constructor() {
         super();
