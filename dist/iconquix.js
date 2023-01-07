49410270b3962611f2ca59d3096fa1e72bbff30e
Fatih Taşkın <mfatih.taskin@gmail.com>
2023-01-07 03:20:26 +0300

    feat(icons): all icons edited

--- dist/iconquix.js
+++ dist/iconquix.js
@@ -5,7 +5,7 @@ class iconQuix extends HTMLElement {
     render() {
         const attrList =this.getAttributeNames();
         const iconParent = this.parentNode;
-        const svgString = iconQuixData[this.getAttribute('icon')] ? `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64">${iconQuixData[this.getAttribute('icon')]}</svg>` : `<svg class="noicon-selected" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64">${iconQuixData['noicon-selected']}</svg>`;
+        const svgString = iconQuixData[this.getAttribute('icon')] ? `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64">${iconQuixData[this.getAttribute('icon')]}</svg>` : `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64">${iconQuixData['noicon-selected']}</svg>`;
         let svgElement = new DOMParser().parseFromString(svgString, 'image/svg+xml').querySelector('svg');
         if (attrList.length > 0) {
             attrList.forEach((attrItem) => {
